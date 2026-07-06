16769130ff6acad838b37147a4e6d5ce426ae54b
Ozkan <oskansubay86@gmail.com>
2026-07-06 11:32:51 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace helloApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
